2c96a047d9e24e0fda438daf2ea516c53258d185
hosoya08 <opsion.08@gmail.com>
2023-01-16 00:36:28 +0900

    レスポンシブメニューを全ページへ反映　完了

--- js/check.js
+++ js/check.js
@@ -1,15 +1,20 @@
 // ALL CHECk
 
-const oyaBtn = document.getElementById("parente");
-const childBtn = document.querySelectorAll(".ECM_CheckboxInput.child");
-
+const oyaBtn = document.getElementById("parente")
+const childBtn = document.querySelectorAll(".ECM_CheckboxInput.child")
 
 $(oyaBtn).on("click", function () {
-    childBtn.forEach((child) => {
-        $(oyaBtn).on("click");
-        console.log(child);
-        child.click();
-        // $(oyaBtn).off("click");
-  });
-});
+  childBtn.forEach((child) => {
+    $(oyaBtn).on("click")
+    console.log(child)
+    child.click()
+    // $(oyaBtn).off("click");
+  })
+})
 
+const navBtn = document.querySelector(".res_nav-btn")
+const spNav = document.querySelector(".menu_section")
+
+navBtn.addEventListener("click", function () {
+  spNav.classList.toggle("active")
+})
